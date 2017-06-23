82927c7a85df654773dc01ba997b8afd22eb9623
samgsmall <samsmall28a@gmail.com>
2017-06-23 11:10:38 +0100

    Update login.js

--- website/login.js
+++ website/login.js
@@ -5,7 +5,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "Formget" && password == "formget#123"){
 alert ("Login successfully");
-window.location = "Index.html";
+window.location = "index.html";
 return false;
 }
 else{
